79b1bae7336ddf7ace8a3c3c477b7bbca715c4a7
Rohit Bansal <comp.rohit@gmail.com>
2017-10-02 17:35:10 +0800

    init

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ===== MODULES ===============================================================
-var express = require('express');
+import express from 'express';
 // ===== MESSENGER =============================================================
 //import ThreadSetup from './messenger-api-helpers/thread-setup';
 
@@ -19,16 +19,16 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-//app.use(function(err, req, res) {
-  // set locals, only providing error in development
-  //res.locals.message = err.message;
-  //res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res) {
+  //set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  //res.status(err.status || 500);
+  //render the error page
+  res.status(err.status || 500);
   //res.render('error');
-  //res.send('error')
-//});
+  res.send('error')
+});
 
 
 /* ----------  Messenger setup  ---------- */
--- bin/www.js
+++ bin/www.js
@@ -6,7 +6,11 @@
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+ require('babel-register')({
+   presets: ['latest', 'react'],
+   plugins: ['transform-class-properties'],
+ });
+ 
 // ===== MODULES ===============================================================
 const app = require('../app');
 const http = require('http');
