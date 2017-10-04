4a113bf31fabe057623db857b8e92472a7eb1f41
Rohit Bansal <comp.rohit@gmail.com>
2017-10-04 21:05:30 +0800

    init

--- messenger-api-helpers/messages.js
+++ messenger-api-helpers/messages.js
@@ -3,23 +3,25 @@ const SERVER_URL = process.env.SERVER_URL;
 /**
  * The Get Started button.
  */
-const getStarted =[ {
-  setting_type: 'call_to_actions',
-  thread_state: 'new_thread',
-  call_to_actions: [
-    {
-      payload: JSON.stringify({
-        type: 'GET_STARTED',
-      }),
-    },
-  ],
-},
-{
-  setting_type: 'greeting',
-  greeting: {
-    text: 'Hi {{user_first_name}}, Welcome to this travel bot',
-  }
-},
+const getStarted =
+[
+  {
+    setting_type: 'call_to_actions',
+    thread_state: 'new_thread',
+    call_to_actions: [
+      {
+        payload: JSON.stringify({
+          type: 'GET_STARTED',
+        }),
+      },
+    ],
+  },
+  {
+    setting_type: 'greeting',
+    greeting: {
+      text: 'Hey {{user_first_name}}, Welcome! I am travel BOT ...',
+    }
+  },
 ]
 
 export default {
