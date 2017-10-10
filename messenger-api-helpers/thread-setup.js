3288a71220a0db37db0e478a3e1e9a34f2d9f909
Rohit Bansal <comp.rohit@gmail.com>
2017-10-10 20:37:32 +0800

    menu

--- messenger-api-helpers/thread-setup.js
+++ messenger-api-helpers/thread-setup.js
@@ -30,7 +30,7 @@ const setDomainWhitelisting = () => {
   api.callThreadAPI(
     {
       setting_type: 'domain_whitelisting',
-      whitelisted_domains: [SERVER_URL],
+      whitelisted_domains: [SERVER_URL, 'https://www.google.com'],
       domain_action_type: 'add',
     },
     {
