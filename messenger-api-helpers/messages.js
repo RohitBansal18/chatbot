b84a8e511e04d85ce3fbb18f100b4547b4477216
Rohit Bansal <comp.rohit@gmail.com>
2017-10-10 22:33:40 +0800

    menu

--- messenger-api-helpers/messages.js
+++ messenger-api-helpers/messages.js
@@ -61,10 +61,8 @@ const myAccountButton = {
  * The persistent menu for users to use.
  */
 const persistentMenu = {
-  //locale:"default",
-  //composer_input_disabled: true,
-  setting_type: 'call_to_actions',
-  thread_state: 'existing_thread',
+  locale:"default",
+  composer_input_disabled: true,
   call_to_actions: [
     {
       type: 'web_url',
